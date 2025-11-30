2c81d3602b14cc47e6e801e06c690574b2265175
Sanjana S <sanj@Mac.fritz.box>
2025-11-30 14:30:52 +0100

    portfolio link

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Portfolio/',
   plugins: [vue()],
   server: {
     port: 3000,
